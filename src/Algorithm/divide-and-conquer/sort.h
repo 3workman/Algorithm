c3479192a81e28a4315b3674e71bd47205b2d97e
3workman <734688714@qq.com>
2019-04-06 16:21:12 +0800

    线性排序：计数/基数/桶

--- src/Algorithm/divide-and-conquer/sort.h
+++ src/Algorithm/divide-and-conquer/sort.h
@@ -23,8 +23,10 @@
 #pragma once
 #include <vector>
 
+// ------------------------------------------------------------
+// 插入 Θ(n^2)
 template <typename T> void SortInsert(std::vector<T>& arr) {
-	for (int i = 1; i < arr.size(); ++i) {
+	for (size_t i = 1; i < arr.size(); ++i) {
 		T key = arr[i];	//待比较的新元素
 		int j = i - 1;	//新元素之前的，已经排序好了
 		while (j >= 0 && arr[j] > key) {
@@ -36,7 +38,7 @@ template <typename T> void SortInsert(std::vector<T>& arr) {
 }
 
 // ------------------------------------------------------------
-// 归并
+// 归并 Θ(nlgn)
 template <typename T> void SortMerge(std::vector<T>& arr) { SortMerge(arr, 0, arr.size()); }
 template <typename T> void SortMerge(std::vector<T>& arr, int begin, int end) {
 	if (end - begin > 1) {
@@ -58,7 +60,7 @@ template <typename T> void SortMerge2(std::vector<T>& arr, int begin, int mid, i
 }
 
 // ------------------------------------------------------------
-// 快排
+// 快排 Θ(nlgn)：算法成熟常数因子很小，cpu cache友好
 template <typename T> void SortQuick(std::vector<T>& arr) { SortQuick(arr, 0, arr.size()); }
 template <typename T> void SortQuick(std::vector<T>& arr, int begin, int end) {
 	if (end - begin > 1) {
@@ -66,14 +68,84 @@ template <typename T> void SortQuick(std::vector<T>& arr, int begin, int end) {
 		SortQuick(arr, begin, pivot);
 		SortQuick(arr, pivot+1, end);
 	}
+	//while (end - begin > 1) { //尾递归，减少栈深度
+	//	int pivot = SortQuick2(arr, begin, end);
+	//	SortQuick(arr, begin, pivot);
+	//	begin = pivot + 1;
+	//}
 }
-template <typename T> int SortQuick2(std::vector<T>& arr, int begin, int end) { 
-	int& pivot = arr[begin]; //定主元，用以分割……TODO:optimize:随机选取主元，交换至排头
-	int i = begin; //i之前是小于等于主元的
-	for (int i = begin + 1; i < end; ++i) {
-		if (arr[i] <= pivot) std::swap(arr[++i], arr[i]);
-		//循环不变式：idx之前<=pivot，idx到i>pivot，i之后待检测
+template <typename T> int SortQuick2(std::vector<T>& arr, int begin, int end) {
+	//std::swap(arr[begin], arr[Rand::rand(begin, end)]);
+	int& pivot = arr[begin]; //定主元，用以分割……Optimize:随机选取主元，交换至排头；三数取中；
+	int i = begin; //i之前是小于等于主元的，所以交换的是i+1元素
+	for (int j = begin + 1; j < end; ++j) {
+		if (arr[j] <= pivot) std::swap(arr[++i], arr[j]);
+		//循环不变式：i之前<=pivot，i到j>pivot，j之后待检测
 	}
 	std::swap(pivot, arr[i]); //主元交换到正确的位置
 	return i;
+}
+
+// ------------------------------------------------------------
+// 堆排序，最大堆 Θ(nlgn)
+#define Left(i)		((i<<1)+1)
+#define Right(i)	((i<<1)+2)
+#define Parent(i)	((i-1)>>1)
+template <typename T> void SortHeap(std::vector<T>& arr) {
+	//构建最大堆
+	int heapSize = arr.size();
+	for (int i = Parent(heapSize-1); i >= 0; --i) { //从最后一个元素的父结点开始调整
+		HeapIfy(arr, i, heapSize);
+	}
+	for (--heapSize; heapSize > 0; --heapSize) {//减少堆size
+		std::swap(arr[0], arr[heapSize]);		//根节点交换至尾部
+		HeapIfy(arr, 0, heapSize);				//调整新堆
+	}
+}
+template <typename T> void HeapIfy(std::vector<T>& arr, int i, const int kHeapSize) {
+	int l = Left(i), r = Right(i), big = i;
+	if (l < kHeapSize && arr[l] > arr[big]) big = l;
+	if (r < kHeapSize && arr[r] > arr[big]) big = r;
+	if (big != i) {
+		std::swap(arr[big], arr[i]);
+		HeapIfy(arr, big, kHeapSize);
+	}
+}
+#undef Left
+#undef Right
+#undef Parent
+
+// ------------------------------------------------------------
+// 计数排序 Θ(n+k)：输入都属于一个小区间中的整数 [range1, range2]
+template <typename T> void SortCount(std::vector<T>& arr, T range1, T range2) {
+	std::vector<T> ret(arr.size());
+	std::vector<T> range(range2-range1+1);
+	for (auto & it : arr) ++range[ it-range1 ];
+	for (size_t i = 1; i < range.size(); ++i) range[i] += range[i-1]; //range[i]记录有多少个元素<=i
+	for (size_t i = arr.size()-1; i >= 0; --i) { //倒序遍历，保证排序是稳定的
+		auto rangeIdx = arr[i]-range1;
+		ret[ --range[rangeIdx] ] = arr[i]; //计数减一，即元素要放入的位置
+	}
+	arr = std::move(ret);
+}
+
+// ------------------------------------------------------------
+// 基数排序 Θ(d(n+k))：n个d位元素3
+// 先对个位稳定排序，再对十位，再对百位……可对多关键字的记录排序，如日期(年月日)
+#define Radix(n, d) ((n%std::pow(10,d))/std::pow(10,d-1))
+#undef Radix
+
+// ------------------------------------------------------------
+// 桶排序 Θ(n)：元素均匀分布于[range1, range2]
+#define BucketIdx(n) (n-range1)*(arr.size()-1)/(range2-range1)
+template <typename T> void SortBucket(std::vector<T>& arr, T range1, T range2) {
+	std::vector<std::vector<T>> buckets(arr.size());
+	for (auto& it : arr) {
+		buckets[BucketIdx(it)].push_back(it); //按元素大小，分布到桶中
+	}
+	arr.resize(0);
+	for (auto& it : buckets) {
+		SortInsert(it);								//对每个桶排序
+		for (auto& iter : it) arr.push_back(iter);	//依次将桶组装
+	}
 }
--- src/Algorithm/main.cpp
+++ src/Algorithm/main.cpp
@@ -1,9 +1,35 @@
+/***********************************************************************
+* @ 思路分析，解决问题的步骤
+	·重现如何分析问题，如何一步步解决的
+
+* @ 一、先罗列要实现的几个点，再编码
+	·方便最后检查时的功能遗漏，如
+		·memcpy忘返回原始指针
+		·链表reverse返回新头节点
+
+* @ 二、循环不变式，构造、证明
+	·设计循环不变式
+	·证明不变式初始化时为真
+	·每次迭代都维持不变
+		·不直观的规律，利用数学归纳法
+		·递归式 -> 尾递归 -> 循环
+		·每种的思路分析，细致明了的写出来
+
+* @ author zhoumf
+* @ date 2019-4-6
+************************************************************************/
 #include "stdafx.h"
+#include "divide-and-conquer/max_sub_array.h"
+#include "divide-and-conquer/sort.h"
 
 int main(int argc, char* argv[])
 {
-    LogFile log("log/algorithm", LogFile::DEBUG);
-    _LOG_MAIN_(log);
+	//LogFile log("log/algorithm", LogFile::DEBUG);
+	//_LOG_MAIN_(log);
+
+	std::vector<int> arr{-9,-1,-2, 5, 1, 6, 0, -11};
+	//std::vector<int> arr{ 1,2,3};
+	SortBucket(arr, -11, 6);
 
     system("pause");
     return 0;
